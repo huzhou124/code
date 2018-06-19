d2f2a78e97e4c6c49df922acbe31ed0babf9590d
Bluescreen Hu <32694111+huzhou124@users.noreply.github.com>
2018-06-19 09:53:10 +0800

    创建查看服务器信息
    基于centos6.7，这个脚本查看一些简单的服务器信息
    （服务器的产品名称，
    SN信息，
    cpu的信息包括：
              1.逻辑cpu个数
              2.物理及实际cpu个数
              3.每个cpu上核数
              4.每个核线程数
    磁盘信息 #注意这是经过配置raid之后的，
    网卡信息

---
+++ server_info.py
@@ -0,0 +1,35 @@
+#！/usr/bin/env python
+# coding=utf-8
+import os
+CMD = """
+    logicalNumber=$(grep "processor" /proc/cpuinfo|sort -u|wc -l)
+    physicalNumber=$(grep "physical id" /proc/cpuinfo|sort -u|wc -l)
+    coreNumber=$(grep "cpu cores" /proc/cpuinfo|uniq|awk -F':' '{print $2}'|xargs)
+    HTNumber=$((logicalNumber / (physicalNumber * coreNumber)))
+    cpu_info=$(cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c)
+    eth_n=$(ip a|grep "192.168"|head -n 1|awk '{print $7}')
+    echo "******* Information *********"
+    echo "`dmidecode | grep "Product Name" `"|awk '{printf "%-s\\n",$0}'|cut -c 2-60
+    echo "${cpu_info}"|cut -c 6-60
+    echo "Logical CPU Number  : ${logicalNumber}"
+    echo "Physical CPU Number : ${physicalNumber}"
+    echo "CPU Core Number     : ${coreNumber}"
+    echo "HT Number           : ${HTNumber}"
+    echo "`fdisk -l |grep -sn "Disk /dev/sd"`"
+    echo "Totalmem            : `cat /proc/meminfo|grep "MemTotal"|awk -F: '{print \$2}'|awk '{print $1,$2}'`"
+    echo "Ethernet info:"
+    echo "Eth  :   ${eth_n}"
+    (lspci > /dev/null 2>&1)||(yum install -y pciutils-3.1.10-4.el6.x86_64 > /dev/null 2>&1)
+    echo `lspci | grep Ethernet|awk -F: '{print $3}'|head -n 1`
+    echo `ethtool ${eth_n}|sed -n '/Speed/p;/Dup/p'|awk '{printf "%s\\n%s\\n",$1,$2}'`
+    echo "*****************************"
+    """
+#假如你的硬盘大于4T就会提示使用gun，而不是使用gtp的东西。
+#第10行这里是取ip192.168开头的
+#注意第22是会去判断是否有lspci这个命令，没有就去下载pciutils-3.1.10-4.el6.x86_64
+        #os.system(CMD)
+for i in os.popen(CMD).readlines():
+    if i.startswith("WA"):
+        pass
+    else:
+        print i.strip()
